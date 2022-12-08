abede9c59e38f7f8a66e7a3e22377b1fa79ce538
ABHIJEET SATYAM <90769313+abhijeetsatyam95@users.noreply.github.com>
2022-12-08 09:27:22 +0530

    PhaseTime
    Video Chat Application

---
+++ backend/executor/executor.py
@@ -0,0 +1,15 @@
+import numpy as np
+from jina import Executor, requests
+
+
+class PhaseTimeExecutor(Executor):
+    last_user_frames = {}
+
+    @requests
+    def foo(self, docs, **kwargs):
+        for d in docs:
+
+            self.last_user_frames[d.tags['user']] = d.tensor
+
+            if len(self.last_user_frames) > 1:
+                d.tensor = np.concatenate(list(self.last_user_frames.values()), axis=0)
